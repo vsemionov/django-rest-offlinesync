ab6cc519ba5c35af9ec2d52de1bb1d67497393fa
Victor Semionov <vsemionov@gmail.com>
2017-09-02 15:31:02 +0300

    basic list test

--- example/api/tests.py
+++ example/api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from .models import Document
+
+
+user = User.objects.create(username='test', password='test')
+document = Document.objects.create(user=user, title='test', text='test')
+
+
+class TestDocuments(APITestCase):
+
+    base_url = reverse('document-list', kwargs={'user_id': user.username})
+
+    def test_list_success(self):
+        response = self.client.get(self.base_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
